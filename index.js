50f2a012b7661729b05971a3c6cc2e699efb6475
Mauro Gutierrez <yoguiimaster@gmail.com>
2022-03-18 17:53:36 -0300

    practicando los metodos

---
+++ index.js
@@ -0,0 +1,82 @@
+var express = require("express");
+var app = express();
+
+app.use("/assets/", express.static(__dirname + "/public"));
+
+app.use("/", function (req, res, next) {
+  console.log("Hicieron un Request a " + req.url);
+  next();
+});
+
+app.get("/", function (req, res) {
+  res.send("Hola");
+});
+
+app.get("/api/:id", function (req, res) {
+  res.json({ parametro: req.params.id });
+});
+
+app.get("/api/:id/:nombre/:valor", function (req, res) {
+  res.json({
+    id: req.params.id,
+    nombre: req.params.nombre,
+    valor: req.params.valor,
+  });
+});
+
+app.get("/ab*cd", function (req, res) {
+  res.send("ab*cd");
+});
+
+app.get("/api", function (req, res) {
+  var obj = {
+    nombre: "prueba",
+    framework: "express",
+    ventaja: "serializó por nosotros",
+  };
+  res.json(obj);
+});
+
+app.get("/static", function (req, res) {
+  res.send(
+    '<html><head> \
+			<link href="/assets/style.css" rel="stylesheet"> \
+			</head><body> \
+				<p>Archivos estaticos rapido y facil!!</p>\
+				<img src="/assets/imagen.jpg">\
+			</body></html>'
+  );
+});
+
+app.get("/datos/", function (req, res) {
+  res.json(req.query);
+});
+
+//-------
+
+app.get("/form", function (req, res) {
+  res.send(
+    '<html><head> \
+			<link href="/assets/style.css" rel="stylesheet"> \
+			</head><body>\
+				<form method="POST" action="/form">\
+				Nombre <input name="nombre" type="text"><br>\
+				Apellido <input name="apellido" type="text"><br>\
+				Curso <input name="curso" type="text"><br>\
+				<input type="submit">\
+				</form>\
+			</body></html>'
+  );
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.post("/form", function (req, res) {
+  res.json(req.body);
+});
+
+app.use(express.json());
+app.post("/formjson", function (req, res) {
+  res.json(req.body);
+});
+
+app.listen(1337);
